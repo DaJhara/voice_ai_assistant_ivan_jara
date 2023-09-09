e3b9691ff22e7d5587e0cd4c77647ecd13713cad
Dajara <dajara92@gmail.com>
2023-09-08 21:08:04 -0500

    Commit 3 Se agrearon las fuentes en assets y el appbar en el main

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:voice_ai_assistant_ivan_jara/pallete.dart';
 import 'home_page.dart';
 
 void main() {
@@ -12,13 +13,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+      title: 'Voice Assistant',
+      theme: ThemeData.light(useMaterial3: true).copyWith(
+        scaffoldBackgroundColor: Pallete.whiteColor,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Pallete.whiteColor),
       ),
       home: const HomePage(),
     );
